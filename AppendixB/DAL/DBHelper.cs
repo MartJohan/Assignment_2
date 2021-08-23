23f0373e310abad03531140c156f945b0c00c048
Tien Nguyen <tien.nguyen@no.experis.com>
2021-08-23 13:25:55 +0200

    created dbhelper

---
+++ AppendixB/DAL/DBHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace dotnetcore.DAL
+{
+    public class DBHelper
+    {
+
+        SqlConnectionStringBuilder a;
+
+    }
+}
